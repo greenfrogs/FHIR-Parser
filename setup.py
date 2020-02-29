50e72915b018a12e56e0956f85b16b23768a992b
Greenfrogs <5961364+greenfrogs@users.noreply.github.com>
2020-02-29 18:49:36 +0000

    Readthedocs yml and config for pypi

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='FHIR-parser',
+    version='0.1.0',
+    author='Greenfrogs',
+    author_email='5961364+greenfrogs@users.noreply.github.com',
+    description='An elegant and simple FHIR library for Python, built for human beings',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/greenfrogs/FHIR-Parser',
+    license='Apache License 2.0',
+    install_requires=['requests>=2.23.0'],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
